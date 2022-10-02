e5df2db438a082d99bc6ad4cc6e4b612069e9559
GODNOEL <ah2650@naver.com>
2022-10-02 15:38:59 +0900

    [Add] IKEA 프로젝트 파일 생성

---
+++ IKEA/IKEA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IKEA
+//
+//  Created by Yu ahyeon on 2022/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
