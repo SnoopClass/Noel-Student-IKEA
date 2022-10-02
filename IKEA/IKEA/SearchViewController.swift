417b6d07f8b44a12f0780f05c856cdbab37c3720
GODNOEL <ah2650@naver.com>
2022-10-02 21:25:26 +0900

    [Add] SearchViewController 파일 생성

---
+++ IKEA/IKEA/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SearchViewController.swift
+//  IKEA
+//
+//  Created by Yu ahyeon on 2022/10/02.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    // MARK: - property
+
+    
+
+    // MARK: - life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - func
+
+}
