dee9efb852d29a11d00e9fde808bdde7f4b66305
GODNOEL <ah2650@naver.com>
2022-10-03 19:03:47 +0900

    [Add] 검색창 UITextField로 구현

--- IKEA/IKEA/SupportingView/SearchBarSectionView.swift
+++ IKEA/IKEA/SupportingView/SearchBarSectionView.swift
@@ -16,25 +16,37 @@ class SearchBarSectionView: UIView {
         $0.translatesAutoresizingMaskIntoConstraints = false
         return $0
     }( UILabel())
-    
+
     let searchBarTextField: UITextField = {
         $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.placeholder = "무엇을 찾고 있나요?"
+        $0.frame = CGRect(x: 0, y: 0, width: 0, height: 100)
+        $0.addLeftPadding()
+        $0.layer.borderColor = UIColor.label.cgColor
+        $0.layer.borderWidth = 1
+        $0.layer.masksToBounds = true
+        $0.layer.cornerRadius = 10
         return $0
     }(UITextField())
-    
+
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .red
         setupLayout()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     // MARK: - Method
     func setupLayout() {
+        setupTitle()
+        setupSearchBar()
+    }
+
+    func setupTitle() {
         self.addSubview(titleLable)
         let titleLableConstraint = [
             titleLable.topAnchor.constraint(equalTo: self.topAnchor, constant: 30),
@@ -42,5 +54,33 @@ class SearchBarSectionView: UIView {
         ]
         NSLayoutConstraint.activate(titleLableConstraint)
     }
+
+    func setupSearchBar() {
+        self.addSubview(searchBarTextField)
+        let searchBarTextFieldConstraint = [
+            searchBarTextField.topAnchor.constraint(equalTo: titleLable.bottomAnchor, constant: 30),
+            searchBarTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
+            searchBarTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30),
+            searchBarTextField.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ]
+        NSLayoutConstraint.activate(searchBarTextFieldConstraint)
+        searchBarTextField.addleftimage(image: UIImage(systemName: "magnifyingglass")!)
+    }
     
+}
+
+extension UITextField {
+    func addLeftPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+    
+    func addleftimage(image:UIImage) {
+        let leftimage = UIImageView(frame: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        leftimage.image = image
+        leftimage.tintColor = .systemGray
+        self.leftView = leftimage
+        self.leftViewMode = .always
+    }
 }
