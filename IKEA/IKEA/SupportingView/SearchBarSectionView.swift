2f8139c4fba921a46284e88d73e2e92913da8be9
GODNOEL <ah2650@naver.com>
2022-10-03 18:01:28 +0900

    [Feat] 검색 title 선언 및 생성

--- IKEA/IKEA/SupportingView/SearchBarSectionView.swift
+++ IKEA/IKEA/SupportingView/SearchBarSectionView.swift
@@ -10,18 +10,37 @@ import UIKit
 class SearchBarSectionView: UIView {
     
     // MARK: - Property
-
+    private let titleLable: UILabel = {
+        $0.text = "검색"
+        $0.font = UIFont.boldSystemFont(ofSize: 22)
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }( UILabel())
+    
+    let searchBarTextField: UITextField = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UITextField())
     
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .red
+        setupLayout()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - Method
-
+    func setupLayout() {
+        self.addSubview(titleLable)
+        let titleLableConstraint = [
+            titleLable.topAnchor.constraint(equalTo: self.topAnchor, constant: 30),
+            titleLable.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30)
+        ]
+        NSLayoutConstraint.activate(titleLableConstraint)
+    }
+    
 }
