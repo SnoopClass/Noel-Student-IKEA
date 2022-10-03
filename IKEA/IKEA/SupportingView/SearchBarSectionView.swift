55e80748d5d90b2a03b74e8719f2622cfe0136c3
GODNOEL <ah2650@naver.com>
2022-10-03 18:00:40 +0900

    [Add] SearchBarSectionView 파일 생성 및 세팅

---
+++ IKEA/IKEA/SupportingView/SearchBarSectionView.swift
@@ -0,0 +1,27 @@
+//
+//  SearchBarSectionView.swift
+//  IKEA
+//
+//  Created by Yu ahyeon on 2022/10/03.
+//
+
+import UIKit
+
+class SearchBarSectionView: UIView {
+    
+    // MARK: - Property
+
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .red
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Method
+
+}
