d5d9064328562dda2ec011726cf150fb063305a1
GODNOEL <ah2650@naver.com>
2022-10-05 17:16:32 +0900

    [Add] RecentProductSectionView 생성

---
+++ IKEA/IKEA/SupportingView/RecentProductSectionView.swift
@@ -0,0 +1,29 @@
+//
+//  RecentProductSectionView.swift
+//  IKEA
+//
+//  Created by Yu ahyeon on 2022/10/05.
+//
+
+import UIKit
+
+class RecentProductSectionView: UIView {
+
+    // MARK: - Property
+
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .red
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Method
+
+
+}
