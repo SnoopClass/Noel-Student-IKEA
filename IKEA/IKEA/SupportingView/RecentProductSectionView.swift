beb9dc0412eba47667d71b53d9e92caf6ad2048c
GODNOEL <ah2650@naver.com>
2022-10-09 22:04:11 +0900

    [Add] collectView 생성 (#3)
     RecentProductSectionView의 최근 물건을 보여줄 collectView을 제작합니다.

--- IKEA/IKEA/SupportingView/RecentProductSectionView.swift
+++ IKEA/IKEA/SupportingView/RecentProductSectionView.swift
@@ -10,8 +10,22 @@ import UIKit
 class RecentProductSectionView: UIView {
 
     // MARK: - Property
+    private lazy var collectView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collectView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectView.delegate = self
+        collectView.dataSource = self
+        collectView.showsHorizontalScrollIndicator = false
+        collectView.register(
+            UICollectionViewCell.self,
+            forCellWithReuseIdentifier: "RecentProductSectionViewCell")
+        
+        return collectView
+    }()
 
-
+    
     // MARK: - Init
 
     override init(frame: CGRect) {
@@ -27,3 +41,26 @@ class RecentProductSectionView: UIView {
 
 
 }
+
+// MARK: - CollectionViewDataSource
+extension RecentProductSectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "RecentProductSectionViewCell",
+            for: indexPath
+        )
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+}
+
+// MARK: - CollectionViewDelegateFlowLayout
+extension RecentProductSectionView: UICollectionViewDelegateFlowLayout{
+    
+}
+
